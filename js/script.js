720b0c82e98f619ed8931d2f2f7811eeb2cd84ed
mohammadzm01 <zm441572@gmail.com>
2024-09-25 12:31:03 +0330

    ok

--- js/script.js
+++ js/script.js
@@ -4,4 +4,24 @@ let person = "john",
 lastName = "adam",
 age = 100;
 let carName;
-console.log('mamali kako');
+const register = {
+    name : 'okjj',
+    lastName : 'hi',
+};
+console.log(register["name"]);
+const someone = {
+    firstName : "jow",
+    lastName : "gandom",
+    fullName : function() {
+        return this.firstName;
+    },
+    otherPerson = {
+        car4 : "ok", 
+        car5 : "you"
+    }
+}
+console.log(someone.otherPerson["car4"])
+let x = "lastName";
+console.log(x)
+delete someone["gandom"];
+console.log(someone.fullName())

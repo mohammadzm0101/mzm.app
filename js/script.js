10ef5e62b00e5ac6df593b734c2c3c27f35cdf93
mohammadzm01 <zm441572@gmail.com>
2024-10-08 20:35:48 +0330

    ok

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
 function mamado(act1, act2) {
     const mamali = {
         character_1: 'abas gholi',
-        charachter_2: 'mamdolAbadi',
-         character_3: [
-            nasro, mena, allah, va, fathon, gharib
-        ]
+        charachter_2: 'mamdo   Abadi'
     }
-    return (mamali.charachter_2 + act1 + "      " + act2);
+    return mamali.charachter_2 + "   " + (24 / 2) + "   " + act1 + "    " + act2;
 }
-sina.change("hor atieh")
-console.log(sina.mother);
-let mamado = {
-    name: "ali"
+console.log(mamado())
+// form private variable use _ ;)
+let _x = mamado()
+const myObject = {
+    first: 'hi',
+    second: 'hello'
 };
-function mamado2(act1, act2){
-    const mamaliKako ={ 
-        age : 31,
-        family : "zarei moghadam",
-        name = {
-            action_1 : 'go',
-       action2 : 20 
-        }
-     } 
-}
+myObject.first_1 = "john";
+myObject.first_1 = "little john";
+console.log(myObject.first_1);
+
+const third_1 = {
+    first: "johny",
+    second: "jacki",
+    // third: function () {
+    //     return this.first + "," + this.second;
+    // },
+    first2: {
+        forExample: "aliens"
+    },
+    // first3: [
+    //     good,
+    //     bad
+    // ]
+};
+let x3 = "";
+const x = third_1;
+x.first = "hiiiiiii";
+delete third_1.first;
+let y = "first2";
+let z = "forExample";
+
+// console.log(third_1.third());
+for (let index in third_1) {
+    x3 += third_1[index]
+};
+console.log(x3)
+const tree = Object.values(third_1);
+console.log(tree);
+let me3 =JSON.stringify(third_1);
+// now del {  & " "} create vertical
+console.log(me3);
